bef7b64ba4d149134d82f95482d690a030379838
Preethi Suresh <91382569+Preethi-Suresh@users.noreply.github.com>
2022-12-29 21:11:12 +0530

    Create DAY9.java

---
+++ 30DAYSOFCODE/DAY9.java
@@ -0,0 +1,53 @@
+/*
+Problem Statement-[Recursion](https://www.hackerrank.com/challenges/30-recursion)
+ */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    
+    public static int factorial(int n) {
+    
+    int i=n-1;
+  
+
+    while(i!=0){
+       
+        n*=i;
+        i--;
+    }
+    int result=n;
+    return result;
+    
+    
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.factorial(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
